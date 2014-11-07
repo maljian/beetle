82e751bef4cd44ccbcc0355261f83188ebc98e84
FarukDoganci <fdoganci94@gmail.com>
2014-11-07 10:04:37 +0100

    joingame und newgame sind nun fertig

--- src/Beetle/Haggis/Client/JoinGame.java
+++ src/Beetle/Haggis/Client/JoinGame.java
@@ -1,27 +1,94 @@
 package Beetle.Haggis.Client;
 
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
 /**
- * @author 
+ * @author Faruk 
  * @version 1.0
  * @created 25-Okt-2014 19:32:33
  */
-public class JoinGame {
 
-	private int btnPlay;
-	private int lblIPAdress;
-	private int lblPlayerName;
-	private int txtIPAdress;
-	private int txtPlayerName;
-	public JoinGameModel m_JoinGameModel;
+public class JoinGame extends JFrame {
 
-	public JoinGame(){
 
+	private JPanel contentPane;
+	private JTextField txtPlayerName;
+	private JTextField txtIpAdress;
+	private JButton btnPlay;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JoinGame frame = new JoinGame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
-	public void finalize() throws Throwable {
-		super.finalize();
+	/**
+	 * Create the frame.
+	 */
+	public JoinGame() {
+		setResizable(false);
+		setTitle("Haggis - Spiel beitreten");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 500);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(178, 34, 34));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtPlayerName = new JTextField();
+		txtPlayerName.setBounds(582, 50, 133, 20);
+		contentPane.add(txtPlayerName);
+		txtPlayerName.setColumns(10);
+		
+		JLabel lblSpielername = new JLabel("Spielername:");
+		lblSpielername.setForeground(new Color(255, 255, 255));
+		lblSpielername.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblSpielername.setBounds(475, 53, 97, 17);
+		contentPane.add(lblSpielername);
+		
+		JLabel lblBitteIpAdresse = new JLabel("Bitte IP-Adresse eingeben:");
+		lblBitteIpAdresse.setForeground(new Color(255, 255, 255));
+		lblBitteIpAdresse.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblBitteIpAdresse.setBounds(475, 98, 240, 20);
+		contentPane.add(lblBitteIpAdresse);
+		
+		txtIpAdress = new JTextField();
+		txtIpAdress.setColumns(10);
+		txtIpAdress.setBounds(475, 129, 240, 20);
+		contentPane.add(txtIpAdress);
+		
+		btnPlay = new JButton("Spielen");
+		btnPlay.setFont(new Font("Arial", Font.PLAIN, 12));
+		btnPlay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnPlay.setBounds(626, 391, 89, 30);
+		contentPane.add(btnPlay);
+		
+		
 	}
-	public void EventHandler(){
+}
 
-	}
-}//end JoinGame
--- src/Beetle/Haggis/Client/NewGame.java
+++ src/Beetle/Haggis/Client/NewGame.java
@@ -1,38 +1,133 @@
 package Beetle.Haggis.Client;
 
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
 import Beetle.Haggis.Client.GameFieldModel.NewGameModel;
 
 /**
- * @author 
+ * @author Faruk
  * @version 1.0
  * @created 25-Okt-2014 19:32:34
  */
 public class NewGame extends JoinGame {
 
+	private JPanel contentPane;
+	private JTextField txtPlayerName;
+	private JButton btnPlay;
+	private JTextField txtIpAdress;
+
 	/**
-	 * cb = comboBox = dropdown
+	 * Launch the application.
 	 */
-	private int cbNumberPlayer;
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					NewGame frame = new NewGame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
 	/**
-	 * cb = comboBox = dropdown
+	 * Create the frame.
 	 */
-	private int cbTargetPoint;
-	private int chckbxBet;
-	private int chckbxBomb;
-	private int lblBet;
-	private int lblBomb;
-	private int lblNumberPlayer;
-	private int lblTargetPoint;
-	public NewGameModel m_NewGameModel;
+	public NewGame() {
+		setTitle("Haggis - Neues Spiel");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 500);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(178, 34, 34));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtPlayerName = new JTextField();
+		txtPlayerName.setBounds(582, 50, 133, 20);
+		contentPane.add(txtPlayerName);
+		txtPlayerName.setColumns(10);
+		
+		JLabel lblPlayerName = new JLabel("Spielername:");
+		lblPlayerName.setForeground(new Color(255, 255, 255));
+		lblPlayerName.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblPlayerName.setBounds(475, 53, 97, 17);
+		contentPane.add(lblPlayerName);
+		
+		JLabel lblNumberPlayer = new JLabel("Anzahl Spieler:");
+		lblNumberPlayer.setForeground(new Color(255, 255, 255));
+		lblNumberPlayer.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblNumberPlayer.setBounds(475, 81, 97, 20);
+		contentPane.add(lblNumberPlayer);
+		
+		btnPlay = new JButton("Spielen");
+		btnPlay.setFont(new Font("Arial", Font.PLAIN, 12));
+		btnPlay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnPlay.setBounds(626, 392, 89, 30);
+		contentPane.add(btnPlay);
+		
+		JComboBox NumberPlayer = new JComboBox();
+		NumberPlayer.setModel(new DefaultComboBoxModel(new String[] {"2", "3"}));
+		NumberPlayer.setFont(new Font("Arial", Font.PLAIN, 12));
+		NumberPlayer.setBounds(684, 81, 33, 20);
+		contentPane.add(NumberPlayer);
+		
+		JLabel lblTargetPoint = new JLabel("Ziel:");
+		lblTargetPoint.setForeground(Color.WHITE);
+		lblTargetPoint.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblTargetPoint.setBounds(475, 112, 97, 20);
+		contentPane.add(lblTargetPoint);
+		
+		JComboBox TargetPoint = new JComboBox();
+		TargetPoint.setModel(new DefaultComboBoxModel(new String[] {"250", "350"}));
+		TargetPoint.setFont(new Font("Arial", Font.PLAIN, 12));
+		TargetPoint.setBounds(660, 112, 57, 20);
+		contentPane.add(TargetPoint);
+		
+		JLabel lblIpAdress = new JLabel("IP-Adresse:");
+		lblIpAdress.setForeground(Color.WHITE);
+		lblIpAdress.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblIpAdress.setBounds(475, 143, 97, 20);
+		contentPane.add(lblIpAdress);
+		
+		txtIpAdress = new JTextField();
+		txtIpAdress.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		try {
+			String ipv4 = InetAddress.getLocalHost().toString();
+			String [] ip = ipv4.split("/");
+			txtIpAdress.setText(ip[1]);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-	public NewGame(){
-
+		txtIpAdress.setEditable(false);
+		txtIpAdress.setColumns(10);
+		txtIpAdress.setBounds(582, 143, 133, 20);
+		contentPane.add(txtIpAdress);
+		
+		
 	}
-
-	public void finalize() throws Throwable {
-		super.finalize();
-	}
-	public void EventHandler(){
-
-	}
-}//end NewGame
+}
