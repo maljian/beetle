814d4d4eb9564909009c009533c8ccb1fe27e683
Nadine <nadine.toepfer@students.fhnw.ch>
2014-11-09 12:02:30 +0100

    Überflüssigen Ordner Beetle gelöscht

--- beetle/README.md
+++
@@ -1,7 +0,0 @@
-beetle
-======
-
-Informatikprojekt 2014
-FHNW Schweiz
-
-Programmieren des Spiels Haggis
--- beetle/src/Rest/Interface1.java
+++
@@ -1,9 +0,0 @@
-package Rest;
-
-/**
- * @version 1.0
- * @created 25-Okt-2014 19:32:33
- */
-public interface Interface1 {
-
-}
