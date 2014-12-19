6d561cb54d985e8f3faa1937747c41b8a17a054b
Nadine <nadine.toepfer@students.fhnw.ch>
2014-12-19 11:08:26 +0100

    Nachtrag Problembehebung

---
+++ src/Beetle/Haggis/Client/GameFieldModel.java
@@ -0,0 +1,255 @@
+package Beetle.Haggis.Client;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.util.ArrayList;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
+import Beetle.Haggis.Message.GameState;
+import Beetle.Haggis.Message.GameState.Combination;
+import Beetle.Haggis.Message.Message;
+//import Beetle.Haggis.Message.Message.MessageType;
+import Beetle.Haggis.Message.Message.PlayedAction;
+import Beetle.Haggis.Network.Card;
+import Beetle.Haggis.Network.Client;
+import Beetle.Haggis.Network.EventHandlerServer;
+import Beetle.Haggis.Network.Player;
+
+//import com.sun.glass.ui.View;
+
+/**
+ * @author Marco Mancuso
+ * @version 1.0
+ * @created 25-Okt-2014 19:32:32
+ */
+public class GameFieldModel {
+
+	GameFieldModel gfModel;
+	protected GameField view;
+	private GameState gState;
+	private int id;
+	private boolean playerIsOnTurn;
+	private Client client;
+
+	public GameFieldModel() {
+		view = new GameField(this);
+		view.setVisible(true);
+		view.btnLegen.setEnabled(false);
+		view.btnPassen.setEnabled(false);
+	}
+
+	public void actualizeView(GameState gs) {
+		// Clear from old items befor draving the new one
+		view.panPlayerCards.removeAll();
+		view.panCenterField.removeAll();
+		view.panJoker.removeAll();
+		view.cardsToCheck.clear();
+		gState = gs;
+		Player[] p = gState.getPlayers();
+		String name = gState.getPlayerName(gState.getPlayerTurns());
+
+		gState.setPlayerPlayed(true, id);
+
+		Stack<Card> playerHandCards = p[id].getCards();
+
+		ArrayList<ButtonCard> btnPlayerCards = new ArrayList<ButtonCard>();
+		ArrayList<ButtonCard> btnJokerCards = new ArrayList<ButtonCard>();
+		ArrayList<ButtonCard> btnPlayedCards = new ArrayList<ButtonCard>();
+
+		for (Card card : playerHandCards) {
+			ButtonCard btnCard = new ButtonCard(card);
+
+			if (card.getValue() >= 2) {
+				btnJokerCards.add((ButtonCard) view.panJoker.add(btnCard));
+			} else {
+				btnPlayerCards.add((ButtonCard) view.panPlayerCards
+						.add(btnCard));
+			}
+			btnCard.addItemListener(view);
+
+		}
+
+		if (gState.getLastPlayedCards() != null) {
+			for (Card card : gState.getLastPlayedCards()) {
+
+				ButtonCard btnCardCenter = new ButtonCard(card);
+				btnPlayedCards.add((ButtonCard) view.panCenterField
+						.add(btnCardCenter));
+			}
+		}
+
+		if (id == gState.getPlayerTurns()) {
+			playerIsOnTurn = true;
+			view.lblHowsTurn.setText("Sie sind am Zug");
+		} else {
+			
+			view.lblHowsTurn.setText(name + " ist am Zug");
+			playerIsOnTurn = false;
+		}
+		
+		enableBtn();
+		
+		updateLabels();
+		view.repaint();
+		view.revalidate();
+	}
+
+	public void anounceWinner(GameState state) {
+		int maxPoint = 0;
+		int idWithMax = 0;
+
+		for (int i = 0; i < state.getPlayers().length; i++) {
+			if (maxPoint < state.getPlayers()[i].getPoints()) {
+				maxPoint = state.getPlayers()[i].getPoints();
+				idWithMax = i;
+			}
+		}
+		String winnerName = state.getPlayers()[idWithMax].getName();
+		view.lblHowsTurn.setText(winnerName + " hat das Spiel mit " + maxPoint
+				+ " Punkten gewonnen.");
+		view.repaint();
+	}
+
+	/**
+	 * @author Faruk
+	 * @return txt
+	 */
+	public String updateLabels() {
+		String txt = null;
+		int opponentTextArea = 2;
+		Player[] p = gState.getPlayers();
+
+		for (Player player : p) {
+
+			txt = player.toString();
+			if (player.getId() == id) {
+
+				view.player1.setText(txt);
+			} else if (opponentTextArea == 2) {
+				view.player2.setText(txt);
+				opponentTextArea++;
+			} else if (opponentTextArea == 3) {
+				view.player3.setText(txt);
+			}
+		}
+		return txt;
+	}
+
+	/**
+	 * zum testen einfach Variable yourTurn auf true oder false setzen false:
+	 * spieler ist nicht an der reihe true: spieler ist an der reihe
+	 * 
+	 * @return false oder true
+	 */
+	public void enableBtn() {
+		if (playerIsOnTurn == true) {
+			view.btnPassen.setEnabled(playerIsOnTurn);
+		} else {
+			view.btnPassen.setEnabled(playerIsOnTurn);
+			view.btnLegen.setEnabled(playerIsOnTurn);
+		}
+	}
+
+	public void setBackgroundColor(Color bgColor) {
+
+		view.player1.setBackground(bgColor);
+		view.player2.setBackground(bgColor);
+		view.player3.setBackground(bgColor);
+		view.buttonsPlace.setBackground(bgColor);
+		view.panPlayerCards.setBackground(bgColor);
+		view.panCenterField.setBackground(bgColor);
+		view.panJoker.setBackground(bgColor);
+		view.helpButtons.setBackground(bgColor);
+		view.opponentField.setBackground(bgColor);
+	}
+
+	public void checkCard(ArrayList<Card> selectedCards) {
+
+		boolean combinationConfirmed = gState.checkCombinations(selectedCards);
+		view.btnLegen.setEnabled(combinationConfirmed && playerIsOnTurn);
+		// TODO 2 knöpfe bereits deaktiviren
+	}
+
+	public void layCards() {
+
+		ArrayList<Card> cardsToPlay = view.cardsToCheck;
+
+		if (gState.checkCombinations(cardsToPlay)) {
+			if (gState.getCurentCombination() == Combination.NEWTURN) {
+				gState.setNewCombination(cardsToPlay);
+			}
+			gState.setLastPlayedCards((ArrayList<Card>) cardsToPlay.clone());
+
+			Stack<Card> playerCards = gState.getPlayers()[id].getCards();
+			for (Card card : cardsToPlay) {
+				playerCards.remove(card);
+			}
+			gState.getPlayers()[id].setCards((Stack<Card>) playerCards.clone());
+
+			System.out.println(gState.getLastPlayedCards().size() + " ");
+			Message m = new Message(gState,
+					PlayedAction.CARDS);// MessageType.CONFIRM,
+			client.sendMessage(m);
+		} else {
+			JOptionPane
+					.showMessageDialog(
+							null,
+							"Es ist nicht möglich die Karten zu legen, überprüfen Sie die Kombination.",
+							"Kombinationsfehler",
+							JOptionPane.INFORMATION_MESSAGE);
+
+		}
+		view.cardsToCheck.clear(); // Gespilte carten aus der zuprüfen liste
+
+	}
+
+	public void pass() {
+		Message m = new Message(gState, PlayedAction.PASS); // MessageType.CONFIRM,
+		client.sendMessage(m);
+	}
+
+	public boolean joinGame(String name, String serverIP) {
+		client = new Client(this);
+		client.connect(name, serverIP);
+		client.start();
+		return true;
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					// GameFieldModel gfm =
+					new GameFieldModel();
+					// GameField frame = new GameField(gfm);
+					// frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public GameState getgState() {
+		return gState;
+	}
+
+	public void setgState(GameState gState) {
+		this.gState = gState;
+	}
+
+	public void setEventHandlerServer(EventHandlerServer ehs) {
+		view.setEventHandlerServer(ehs);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
---
+++ src/Beetle/Haggis/Client/NewGame.java
@@ -0,0 +1,171 @@
+package Beetle.Haggis.Client;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * @author Faruk
+ * @version 1.0
+ * @created 25-Okt-2014 19:32:34
+ */
+public class NewGame extends JoinGame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public JPanel contentPane;
+	protected JTextField txtPlayerName;
+	private JButton btnPlay;
+	public JTextField txtIpAdress;
+	private JPanel logoPanel;
+	private JLabel logoLabel;
+	protected JComboBox<Integer> numberPlayer;
+	protected JComboBox<Integer> TargetPoint; // Problem, deshalb keine Umbenennung!
+	private NewGameModel model;
+
+	/**
+	 * Create the frame.
+	 */
+	// @Override
+	public NewGame(NewGameModel ngm) {
+		super(ngm);
+		model = ngm;
+		setTitle("Haggis - Neues Spiel");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 800, 500);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(178, 34, 34));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		txtPlayerName = new JTextField();
+		txtPlayerName.setBounds(582, 50, 133, 20);
+		contentPane.add(txtPlayerName);
+		txtPlayerName.setColumns(10);
+
+		JLabel lblPlayerName = new JLabel("Spielername:");
+		lblPlayerName.setForeground(new Color(255, 255, 255));
+		lblPlayerName.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblPlayerName.setBounds(475, 53, 97, 17);
+		contentPane.add(lblPlayerName);
+
+		JLabel lblNumberPlayer = new JLabel("Anzahl Spieler:");
+		lblNumberPlayer.setForeground(new Color(255, 255, 255));
+		lblNumberPlayer.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblNumberPlayer.setBounds(475, 81, 97, 20);
+		contentPane.add(lblNumberPlayer);
+
+		btnPlay = new JButton("Spielen");
+		btnPlay.setFont(new Font("Arial", Font.PLAIN, 12));
+		btnPlay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (txtPlayerName.getText().equals("") && e.getSource() == btnPlay) 
+					JOptionPane.showMessageDialog(null, "Bitte geben Sie einen Spielernamen ein.");
+				else
+					model.startServer();
+
+				}
+				
+			}
+		);
+		btnPlay.setBounds(626, 392, 89, 30);
+		contentPane.add(btnPlay);
+
+		numberPlayer = new JComboBox<Integer>();
+		numberPlayer.setModel(new DefaultComboBoxModel(
+				new String[] { "2", "3" }));
+		numberPlayer.setFont(new Font("Arial", Font.PLAIN, 12));
+		numberPlayer.setBounds(684, 81, 33, 20);
+		contentPane.add(numberPlayer);
+
+		JLabel lblTargetPoint = new JLabel("Ziel:");
+		lblTargetPoint.setForeground(Color.WHITE);
+		lblTargetPoint.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblTargetPoint.setBounds(475, 112, 97, 20);
+		contentPane.add(lblTargetPoint);
+
+		TargetPoint = new JComboBox<Integer>();
+		TargetPoint.setModel(new DefaultComboBoxModel(new String[] { "10",
+				"250", "350" }));
+		TargetPoint.setFont(new Font("Arial", Font.PLAIN, 12));
+		TargetPoint.setBounds(660, 112, 57, 20);
+		contentPane.add(TargetPoint);
+
+		JLabel lblIpAdress = new JLabel("IP-Adresse:");
+		lblIpAdress.setForeground(Color.WHITE);
+		lblIpAdress.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblIpAdress.setBounds(475, 143, 97, 20);
+		contentPane.add(lblIpAdress);
+
+		txtIpAdress = new JTextField();
+		txtIpAdress.setHorizontalAlignment(SwingConstants.RIGHT);
+
+		txtIpAdress.setEditable(false);
+		txtIpAdress.setColumns(10);
+		txtIpAdress.setBounds(582, 143, 133, 20);
+		contentPane.add(txtIpAdress);
+
+		logoPanel = new JPanel();
+		logoPanel.setBounds(100, 81, 300, 300);
+		contentPane.add(logoPanel);
+		logoPanel.setLayout(new BorderLayout(0, 0));
+
+		logoLabel = new JLabel("");
+		logoLabel.setIcon(new ImageIcon(getClass().getResource(
+				"/Beetle/Resources/Haggis_Logo_v10.jpg")));
+		logoPanel.add(logoLabel, BorderLayout.CENTER);
+
+		JButton btnCopy = new JButton("kopieren");
+		btnCopy.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Toolkit.getDefaultToolkit()
+						.getSystemClipboard()
+						.setContents(
+								new StringSelection(txtIpAdress.getText()),
+								null);
+			}
+		});
+		btnCopy.setBackground(Color.ORANGE);
+		btnCopy.setBounds(582, 168, 133, 23);
+		contentPane.add(btnCopy);
+
+			
+		
+
+		final JButton btnZurck = new JButton("Zur\u00FCck");
+		btnZurck.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == btnZurck) {
+					model.goBack();
+				}
+			}
+		});
+		btnZurck.setFont(new Font("Arial", Font.PLAIN, 12));
+		btnZurck.setBounds(475, 392, 89, 30);
+		contentPane.add(btnZurck);
+
+	}
+
+	public JComboBox<Integer> getTargetPoint() {
+		return TargetPoint;
+	}
+}
